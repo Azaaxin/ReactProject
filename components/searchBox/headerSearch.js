27bb70d9c95bac6b6df340bb3848853ba44056f4
Azaaxin <ludvig_olausson@hotmail.com>
2020-12-17 08:34:20 +0100

    Fixed typos

--- components/searchBox/headerSearch.js
+++ components/searchBox/headerSearch.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./headerSearch.css";
 export default function HeaderSearchComponent({ name, placeholder, profile }) {
   return (
-    <div class="header_search_area">
+    <div className="header_search_area">
       <span></span>
       <input type="text" placeholder={placeholder}></input>
       <div className="search_go_button">O</div>
