73258668f24575c0f31520104c33e91ea6c9756b
Azaaxin <ludvig_olausson@hotmail.com>
2020-12-17 08:11:23 +0100

    fixed small errors

--- components/parts/headerDesktop/headerLinks.js
+++ components/parts/headerDesktop/headerLinks.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Hlinks({ text, onClick, href }) {
   return (
     <>
-      <div class="header_browse_menu">
+      <div className="header_browse_menu">
         <a href={href} onClick={onClick}>
           {text}
         </a>
--- components/parts/headerDesktop/headerLogo.js
+++ components/parts/headerDesktop/headerLogo.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Hlogo({ name, desc, profile }) {
   return (
     <>
-      <div class="header_logo">Logo</div>
+      <div className="header_logo">Logo</div>
     </>
   );
 }
