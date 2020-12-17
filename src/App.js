e0ccc17b71eca8b7f03988cedd7699ad05a4c4c7
Azaaxin <ludvig_olausson@hotmail.com>
2020-12-17 06:55:40 +0100

    added extra things

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "../components/parts/headerDesktop/header";
 export default function App() {
   return (
     <div className="App">
+      <Header links="Browse" />
       <Cards
         name="Azaaxin"
         avatar="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
